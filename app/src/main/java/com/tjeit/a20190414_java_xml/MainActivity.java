282ee8989a1701c5a8d595df31a5842a6f385b79
Eunsil-Jeong <daramchu@naver.com>
2019-04-14 11:47:47 +0900

    LENGTH_LONG 예시

--- app/src/main/java/com/tjeit/a20190414_java_xml/MainActivity.java
+++ app/src/main/java/com/tjeit/a20190414_java_xml/MainActivity.java
@@ -40,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
                 contentTxtView.setText(userInputStr);
 
                 Toast.makeText(MainActivity.this, "입력값이 반영되었습니다.", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "두번째 토스트", Toast.LENGTH_LONG).show();
             }
         });
 
